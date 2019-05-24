6ad41463be29dca74cbc703a4529e7e21c0c1158
Hugh Morrison <hmorri32@gmail.com>
2019-05-24 15:29:58 -0600

    creates initial data structure for user profile. writes reducers.

--- app/containers/App/reducer.js
+++ app/containers/App/reducer.js
@@ -11,16 +11,22 @@
  */
 
 import { fromJS } from 'immutable';
+import {
+  LOAD_REPOS_SUCCESS,
+  LOAD_REPOS,
+  LOAD_REPOS_ERROR,
+  LOAD_USER_PROFILE_SUCCESS,
+  LOAD_USER_PROFILE,
+  LOAD_USER_PROFILE_ERROR,
+} from './constants';
 
-import { LOAD_REPOS_SUCCESS, LOAD_REPOS, LOAD_REPOS_ERROR } from './constants';
-
-// The initial state of the App
 const initialState = fromJS({
   loading: false,
   error: false,
   currentUser: false,
   userData: {
     repositories: false,
+    profile: false,
   },
 });
 
@@ -37,6 +43,17 @@ function appReducer(state = initialState, action) {
         .set('loading', false)
         .set('currentUser', action.username);
     case LOAD_REPOS_ERROR:
+      return state.set('error', action.error).set('loading', false);
+    case LOAD_USER_PROFILE:
+      return state
+        .set('loading', true)
+        .set('error', false)
+        .setIn(['userData', 'profile'], false);
+    case LOAD_USER_PROFILE_SUCCESS:
+      return state
+        .set('loading', false)
+        .setIn(['userData', 'profile'], action.userProfile);
+    case LOAD_USER_PROFILE_ERROR:
       return state.set('error', action.error).set('loading', false);
     default:
       return state;
