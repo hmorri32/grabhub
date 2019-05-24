fa602d9e87f3d0bec63ce5dd29d7c51caa39f8c9
Hugh Morrison <hmorri32@gmail.com>
2019-05-24 15:33:15 -0600

    writes getuserprofile saga

--- app/containers/HomePage/saga.js
+++ app/containers/HomePage/saga.js
@@ -1,24 +1,18 @@
-/**
- * Gets the repositories of the user from Github
- */
-
 import { call, put, select, takeLatest } from 'redux-saga/effects';
-import { LOAD_REPOS } from 'containers/App/constants';
-import { reposLoaded, repoLoadingError } from 'containers/App/actions';
-
+import { LOAD_REPOS, LOAD_USER_PROFILE } from 'containers/App/constants';
+import {
+  reposLoaded,
+  repoLoadingError,
+  userProfileLoaded,
+  userProfileLoadingError,
+} from 'containers/App/actions';
 import request from 'utils/request';
 import { makeSelectUsername } from 'containers/HomePage/selectors';
 
-/**
- * Github repos request/response handler
- */
 export function* getRepos() {
-  // Select username from store
   const username = yield select(makeSelectUsername());
   const requestURL = `https://api.github.com/users/${username}/repos?type=all&sort=updated`;
-
   try {
-    // Call our request helper (see 'utils/request')
     const repos = yield call(request, requestURL);
     yield put(reposLoaded(repos, username));
   } catch (err) {
@@ -26,13 +20,22 @@ export function* getRepos() {
   }
 }
 
-/**
- * Root saga manages watcher lifecycle
- */
+export function* getUserProfile() {
+  const username = yield select(makeSelectUsername());
+  const requestURL = `https://api.github.com/users/${username}`;
+  try {
+    const userProfile = yield call(request, requestURL);
+    yield put(userProfileLoaded(userProfile));
+  } catch (err) {
+    yield put(userProfileLoadingError(err));
+  }
+}
+
 export default function* githubData() {
   // Watches for LOAD_REPOS actions and calls getRepos when one comes in.
   // By using `takeLatest` only the result of the latest API call is applied.
   // It returns task descriptor (just like fork) so we can continue execution
   // It will be cancelled automatically on component unmount
   yield takeLatest(LOAD_REPOS, getRepos);
+  yield takeLatest(LOAD_USER_PROFILE, getUserProfile);
 }
