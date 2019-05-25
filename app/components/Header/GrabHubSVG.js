e1cd85b6f6803aa0ed85257dcc05f51e037a25fe
Hugh Morrison <hmorri32@gmail.com>
2019-05-25 12:05:13 -0600

    moves grabhub svg inline with header

---
+++ app/components/Header/GrabHubSVG.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export const GrabHubSVG = () => (
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100">
+    <path d="M26.2 35.4l-20 12.5v4.2l20 12.5 2.6-4.2L12.2 50l16.6-10.4zM73.8 35.4l-2.6 4.2L87.8 50 71.2 60.4l2.6 4.2 20-12.5v-4.2z" />
+    <circle cx="50" cy="50" r="5" />
+    <circle cx="65" cy="50" r="5" />
+    <circle cx="35" cy="50" r="5" />
+  </svg>
+);
