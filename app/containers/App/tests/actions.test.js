e08c4ebc209c591a9892ae35198542be03b5e454
Hugh Morrison <hmorri32@gmail.com>
2019-05-25 09:37:54 -0600

    tests user profile actions

--- app/containers/App/tests/actions.test.js
+++ app/containers/App/tests/actions.test.js
@@ -1,6 +1,20 @@
-import { LOAD_REPOS, LOAD_REPOS_SUCCESS, LOAD_REPOS_ERROR } from '../constants';
+import {
+  LOAD_REPOS,
+  LOAD_REPOS_SUCCESS,
+  LOAD_REPOS_ERROR,
+  LOAD_USER_PROFILE,
+  LOAD_USER_PROFILE_SUCCESS,
+  LOAD_USER_PROFILE_ERROR,
+} from '../constants';
 
-import { loadRepos, reposLoaded, repoLoadingError } from '../actions';
+import {
+  loadRepos,
+  reposLoaded,
+  repoLoadingError,
+  loadUserProfile,
+  userProfileLoaded,
+  userProfileLoadingError,
+} from '../actions';
 
 describe('App Actions', () => {
   describe('loadRepos', () => {
@@ -38,6 +52,42 @@ describe('App Actions', () => {
       };
 
       expect(repoLoadingError(fixture)).toEqual(expectedResult);
+    });
+  });
+
+  describe('loadUserProfile', () => {
+    it('should return the correct type', () => {
+      const expectedResult = {
+        type: LOAD_USER_PROFILE,
+      };
+
+      expect(loadUserProfile()).toEqual(expectedResult);
+    });
+  });
+
+  describe('userProfileLoaded', () => {
+    it('should return the correct type and the passed user profile', () => {
+      const fixture = {};
+      const expectedResult = {
+        type: LOAD_USER_PROFILE_SUCCESS,
+        userProfile: fixture,
+      };
+
+      expect(userProfileLoaded(fixture)).toEqual(expectedResult);
+    });
+  });
+
+  describe('userProfileLoadingError', () => {
+    it('should return the correct type and the error', () => {
+      const fixture = {
+        msg: 'ay caramba!',
+      };
+      const expectedResult = {
+        type: LOAD_USER_PROFILE_ERROR,
+        error: fixture,
+      };
+
+      expect(userProfileLoadingError(fixture)).toEqual(expectedResult);
     });
   });
 
