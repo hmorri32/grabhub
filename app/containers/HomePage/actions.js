d5c10e2eb9d325363ae38ea575ed2ad4748888a0
Hugh Morrison <hmorri32@gmail.com>
2019-05-26 17:16:31 -0600

    deletes some whitespace

--- app/containers/HomePage/actions.js
+++ app/containers/HomePage/actions.js
@@ -19,9 +19,7 @@ import { CHANGE_USERNAME } from './constants';
 
 /**
  * Changes the input field of the form
- *
  * @param  {name} name The new text of the input field
- *
  * @return {object}    An action object with a type of CHANGE_USERNAME
  */
 export function changeUsername(name) {
