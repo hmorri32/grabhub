589ab0763779230e270d97b75ab0a60e86ca3345
Hugh Morrison <hmorri32@gmail.com>
2019-05-26 17:57:57 -0600

    styles scrollbar due to why not

--- app/components/List/Ul.js
+++ app/components/List/Ul.js
@@ -7,6 +7,23 @@ const Ul = styled.ul`
   max-height: 30em;
   overflow-y: auto;
   padding: 0 1em;
+
+  ::-webkit-scrollbar {
+    width: 10px;
+    background: #a76664;
+  }
+
+  ::-webkit-scrollbar-track {
+    background: #f1f1f1;
+  }
+
+  ::-webkit-scrollbar-thumb {
+    background: #a76664;
+  }
+
+  ::-webkit-scrollbar-thumb:hover {
+    background: #faa353;
+  }
 `;
 
 export default Ul;
