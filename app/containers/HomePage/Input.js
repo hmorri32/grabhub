0001fc5aad0e5c6baccd4cd94393c2d43a9b7ea5
Hugh Morrison <hmorri32@gmail.com>
2019-05-27 16:44:44 -0600

    adds some focus styles to input because why not

--- app/containers/HomePage/Input.js
+++ app/containers/HomePage/Input.js
@@ -3,6 +3,10 @@ import styled from 'styled-components';
 const Input = styled.input`
   outline: none;
   border-bottom: 1px dotted #999;
+  &:focus {
+    border-bottom: 1px solid #a76664;
+    color: #a76664;
+  }
 `;
 
 export default Input;
