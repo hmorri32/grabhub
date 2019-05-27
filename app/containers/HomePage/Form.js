cd1479cc1bffcff344f2aea91c956c63f578d148
Hugh Morrison <hmorri32@gmail.com>
2019-05-27 16:49:29 -0600

    wee form styling

--- app/containers/HomePage/Form.js
+++ app/containers/HomePage/Form.js
@@ -2,6 +2,18 @@ import styled from 'styled-components';
 
 const Form = styled.form`
   margin-bottom: 1em;
+  font-size: 20px;
+  border-radius: 4px;
+  -webkit-font-smoothing: antialiased
+  -webkit-touch-callout: none;
+  text-decoration: none;
+  user-select: none;
+  cursor: pointer;
+  label {
+    font-family: 'Helvetica Neue', Helvetica, Arial, sans-serif;
+  }
+  font-size: 20px;
+  color: #a76664;
 `;
 
 export default Form;
