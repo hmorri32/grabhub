b3407370fe75479fefac214ec4d94b868f010cad
Hugh Morrison <hmorri32@gmail.com>
2019-05-27 13:33:37 -0600

    deletes comments

--- app/containers/HomePage/saga.js
+++ app/containers/HomePage/saga.js
@@ -49,10 +49,6 @@ export function* getUserFollowers() {
 }
 
 export default function* githubData() {
-  // Watches for LOAD_REPOS actions and calls getRepos when one comes in.
-  // By using `takeLatest` only the result of the latest API call is applied.
-  // It returns task descriptor (just like fork) so we can continue execution
-  // It will be cancelled automatically on component unmount
   yield takeLatest(LOAD_REPOS, getRepos);
   yield takeLatest(LOAD_USER_PROFILE, getUserProfile);
   yield takeLatest(LOAD_USER_FOLLOWERS, getUserFollowers);
