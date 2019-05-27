548c1562807a9c47c735d4e920dda3c6da90a4c0
Hugh Morrison <hmorri32@gmail.com>
2019-05-27 16:44:12 -0600

    styles follwerslist

--- app/components/FollowersList/index.js
+++ app/components/FollowersList/index.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-
+import styled from 'styled-components';
 import FollowerListItem from 'containers/FollowerListItem';
+import H2 from 'components/H2';
 import List from './List';
 import ListWrapper from './ListWrapper';
-// todo: maybe make this a pure component. idk
+
 function FollowersList({ followers }) {
-  if (followers) {
+  const BrandedH2 = styled(H2)`
+    margin: 0 10px 10px 10px;
+    font-family: 'Helvetica Neue', Helvetica, Arial, sans-serif;
+    font-weight: bold;
+    font-size: 22px;
+    color: #a76664;
+  `;
+
+  if (followers.length > 0) {
     return (
       <ListWrapper>
         <List items={followers} component={FollowerListItem} />
@@ -14,7 +23,7 @@ function FollowersList({ followers }) {
     );
   }
 
-  return null;
+  return <BrandedH2>USER HAS NO FOLLOWERS :(</BrandedH2>;
 }
 
 FollowersList.propTypes = {
